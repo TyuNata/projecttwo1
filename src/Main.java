e7b4658c3b0d6c7097724a21dcdcf00ebdb197eb
Tyunat <020182tnm@gmail.com>
2024-05-26 18:00:37 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 500; // сумма баланса
+        int deposit = 1500; // сумма пополнения
+        int total = balance + deposit; // баланс + пополнение
+        int bonus; // бонус
+        if (deposit > 1000) {
+            bonus = deposit / 100;
+        }
+else {bonus = 0;}
+            System.out.println("Ваш счет: "+ total + " бонус: " + bonus);
+    }
+}
